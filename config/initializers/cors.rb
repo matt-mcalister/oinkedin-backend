de64725747993b7eef6719b132a29561e245d22e
github username <github email address>
2018-07-19 14:37:58 -0400

    added appropriate preface to cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'oinked-in.herokuapp.com'
+    origins 'http://oinked-in.herokuapp.com'
 
     resource '*',
       headers: :any,
