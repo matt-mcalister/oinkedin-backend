ca5d8e539a8a2511889dd57dca2957628aebec34
github username <github email address>
2018-07-19 14:44:34 -0400

    readded cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://oinked-in.herokuapp.com'
 
     resource '*',
       headers: :any,
